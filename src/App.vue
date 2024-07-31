9711c75c3bf7d8001b6e6b49e0f39d769027927b
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-31 16:50:32 +0200

    [FEAT] Script can now receive data from the api

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
-<script setup>
+<script>
+import { store } from './store.js' //importo il file store.js
+import axios from 'axios';
 
+export default {
+  data() {
+    store
+  },
+  created() {
+    this.getCards();
+  },
+  methods: {
+    //creo una funzione per popolare l'array che conterrà le carte all'interno di store.js
+    getCards() {
+      axios.get(store.apiUrl).then((data) => {
+        store.cardsList = data.data;
+        //console.log(store.cardsList)
+      })
+    },
+  },
+}
 </script>
 
 <template>
+  <div>
 
+  </div>
 </template>
 
 <style lang="scss" scoped>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    apiPath: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0'
+    apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+    cardsList: [],
 });

ba79576492609d2fd78bb9ac2ab7c02edefae7bf
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-08-01 15:16:07 +0200

    [FEAT] Script can now print something into dom

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,12 @@
 <script>
-import { store } from './store.js' //importo il file store.js
-import axios from 'axios';
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
 
 export default {
-  data() {
-    store
-  },
   components: {
     AppHeader,
     AppMain
-  },
-  created() {
-    this.getCards();
-  },
-  methods: {
-    //creo una funzione per popolare l'array che conterrà le carte all'interno di store.js
-    getCards() {
-      axios.get(store.apiUrl).then((data) => {
-        store.cardsList = data.data;
-        //console.log(store.cardsList)
-      })
-    },
-  },
+  }
 }
 </script>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,36 @@
 <script>
-export default {
+import { store } from '../store.js';
+import axios from 'axios';
 
+export default {
+    data() {
+    return{
+        store
+    }
+  },
+  created() {
+    this.getCards();
+  },
+  methods: {
+    //creo una funzione per popolare l'array che conterrà le carte all'interno di store.js
+    getCards() {
+      axios.get(store.apiUrl).then((response) => {
+        store.cardsList = response.data.data;
+        console.log(store.cardsList)
+      });
+    },
+  },
 }
 </script>
 
 <template>
     <main>
         <div class="container">
-
+            <div class="content">
+                <div class="column-5" v-for="card in store.cardsList" :key="card.id">
+                    {{ card.name }}
+                </div>
+            </div>
         </div>
     </main>
 </template>
@@ -19,6 +42,20 @@ main {
     .container {
         max-width: 1200px;
         margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .content{
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            background-color: white;
+
+            .column-5{
+                width: calc(100% / 5);
+            }
+        }
     }
 }
 </style>
